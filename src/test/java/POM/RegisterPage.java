0d3e4ca8188e97fb45dd36b0dfba28801eb0754c
LinhNhat20303 <phamnhatlinh20303@gmail.com>
2023-10-30 07:58:25 +0700

    poo.xmlisme

---
+++ src/test/java/POM/RegisterPage.java
@@ -0,0 +1,50 @@
+package POM;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import javax.print.DocFlavor;
+
+public class RegisterPage {
+    WebDriver driver;
+
+    By myAccountLink = By.linkText("MY ACCOUNT");
+    By createAccountLink = By.linkText("CREATE AN ACCOUNT");
+    By firstNameInputLocator = By.id("firstname");
+    By lastNameInputLocator = By.id("lastname");
+    By emailInputLocator = By.id("email_address");
+    By passwordInputLocator = By.id("password");
+    By confirmPasswordInputLocator = By.id("confirmation");
+    By registerButton = By.xpath("//button[@title='Register']");
+
+    public void enterFirstName(String firstName) {
+        WebElement firstNameElement = driver.findElement(firstNameInputLocator);
+        firstNameElement.clear();
+        firstNameElement.sendKeys(firstName);
+    }
+
+    public void enterLastName(String lastname) {
+        WebElement lastNameElement = driver.findElement(lastNameInputLocator);
+        lastNameElement.clear();
+        lastNameElement.sendKeys(lastname);
+    }
+
+    public void enterEmail(String email) {
+        WebElement emailElement = driver.findElement(emailInputLocator);
+        emailElement.clear();
+        emailElement.sendKeys(email);
+    }
+
+    public void enterPassword(String password) {
+        WebElement passwordElement = driver.findElement(passwordInputLocator);
+        passwordElement.clear();
+        passwordElement.sendKeys(password);
+    }
+
+    public void enterConfirmPassword(String confirmPassword) {
+        WebElement confirmPasswordElement = driver.findElement(confirmPasswordInputLocator);
+        confirmPasswordElement.clear();
+        confirmPasswordElement.sendKeys(confirmPassword);
+    }
+}
